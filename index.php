9b68e60ffe8af035dfd2eab9bb642d1982449d85
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-12-02 22:34:38 -0600

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include "view-header.php";
+?>
+
+
+<footer>
+    <p> Davids Amorofi</p>
+</footer>
+    
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+<script src="custom.js"></script>
+</body>
+</html>
