1832f27cd38663622b35d08cf86f2d4bb3f72e88
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-12-06 11:02:05 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -72,7 +72,7 @@
         <ul class="navbar-nav ml-auto">
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            List of Cities we are working 
+            List of Cities we are located
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="#">Dallas (Tx)</a>
