582fd85d989a8fc503fa386ad0c6c1b855fee653
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-12-12 20:31:19 -0600

    Update book-appointment.php

--- book-appointment.php
+++ book-appointment.php
@@ -3,7 +3,6 @@ function get_db_connection(){
     
     $conn = new mysqli('159.89.47.44', 'amorofio_davprojectuser', '_pt?kBE.j0[i', 'amorofio_davproject');
     
-  
     if ($conn->connect_error) {
       return false;
     }
@@ -20,16 +19,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $name = htmlspecialchars($name);
     $email = filter_var($email, FILTER_VALIDATE_EMAIL) ? $email : null;
-    
-
 
     $conn = get_db_connection();
-
 
     if ($conn === false) {
         die("Database connection failed");
     }
-
 
     $sql = "INSERT INTO Appointments (ClientName, ContactEmail, AppointmentDate) VALUES ('$name', '$email', '$date $time')";
     
@@ -38,7 +33,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
 
     $conn->close();
 }
